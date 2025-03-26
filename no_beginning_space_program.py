82ac03c62f423a6e7f1c3be130686df3e2638d79
Chris <chriswell.mangahas@gmail.com>
2025-03-26 18:28:23 +0800

    Removed an old version of no_beginning_spaces_program

--- no_beginning_space_program.py
+++
@@ -1,7 +0,0 @@
-#Batch 6 - Program 1
-
-#ask the user to input a string with spaces in the beginning, and initialize a counter for the beginning spaces
-
-#loop through the input to count the beginning spaces by finding the index where the first non-space character appears in the string
-
-#create and display a new string without the beginning spaces by starting from the first non-space character
