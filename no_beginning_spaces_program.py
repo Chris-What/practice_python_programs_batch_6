17f42ef3048169c8bdc21769a21738b40f17431f
Chris <chriswell.mangahas@gmail.com>
2025-03-26 18:25:50 +0800

    Added first lines of code in Batch 6 - Program 1: no_beginning spaces_program, and updated its respective pseudocode to be more detailed.

---
+++ no_beginning_spaces_program.py
@@ -0,0 +1,9 @@
+#Batch 6 - Program 1
+
+#ask the user to input a string with spaces in the beginning and initialize a counter for the spaces
+string = input("Enter a string with spaces in the beginning: ")
+spaces = 0
+
+#remove the beginning spaces without using lstrip()
+
+#display new string
