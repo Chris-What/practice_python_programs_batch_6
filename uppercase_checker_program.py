40f2bb3914d551a77219279205c3feb6bd4f4d82
Chris <chriswell.mangahas@gmail.com>
2025-03-29 00:23:48 +0800

    Added pseudocode for Batch 6 - Program 4: uppercase_checker_prgoram

---
+++ uppercase_checker_program.py
@@ -0,0 +1,6 @@
+#Batch 6 - Program 4
+#Check if the string entered is fully uppercase without using isupper()
+
+#ask the user to input a string
+#check if all characters are in uppercase
+#display the result
