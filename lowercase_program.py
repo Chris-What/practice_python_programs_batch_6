417cc5bfc57d029ff10518741fcb2301e55885fb
Chris <chriswell.mangahas@gmail.com>
2025-03-29 00:07:34 +0800

    Added first lines of code in lowercase_program

--- lowercase_program.py
+++ lowercase_program.py
@@ -2,5 +2,7 @@
 #Convert a string into lowercase without using lower()
 
 #ask the user to input a string with uppercase letters
+string = input("Enter a string with uppercase letters: ")
+
 #convert the uppercase letters in the string into lowercase
 #display the new string
