199ecdf1b27d4bc89b1555c42907ac89f07a28d9
Chris <chriswell.mangahas@gmail.com>
2025-03-29 00:26:41 +0800

    Added first line of code in uppercase_checker_program

--- uppercase_checker_program.py
+++ uppercase_checker_program.py
@@ -2,5 +2,7 @@
 #Check if the string entered is fully uppercase without using isupper()
 
 #ask the user to input a string
+string = input("Enter a string: ")
+
 #check if all characters are in uppercase
 #display the result
