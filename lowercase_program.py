6d7b080c215ecd877c63f453f3676c7eefc0a94a
Chris <chriswell.mangahas@gmail.com>
2025-03-29 00:04:57 +0800

    Added pseudocode for Batch 6 - Program 3: lowercase_program

---
+++ lowercase_program.py
@@ -0,0 +1,6 @@
+#Batch 6 - Program 3
+#Convert a string into lowercase without using lower()
+
+#ask the user to input a string with uppercase letters
+#convert the uppercase letters in the string into lowercase
+#display the new string
