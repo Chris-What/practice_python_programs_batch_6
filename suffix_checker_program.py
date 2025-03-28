23614f1f7f391aa539224340bcc659b93299f730
Chris <chriswell.mangahas@gmail.com>
2025-03-29 01:05:22 +0800

    Added final lines of code in suffix_checker_program, and updated its respective pseudocode.

--- suffix_checker_program.py
+++ suffix_checker_program.py
@@ -5,5 +5,14 @@
 string = input("Enter a string: ")
 suffix = input("Enter suffix you want to check: ")
 
-#check if the end of the string matches with the suffix
-#display the result
+#check if the end of the string matches with the suffix by comparing them
+if len(suffix) > len(string):
+    suffix_check = False
+else:
+    suffix_check = (string[-len(suffix):] == suffix)
+
+#display the result depending if the suffix matches the end of the string or not
+if suffix_check:
+    print("The string ends with the entered suffix.")
+else:
+    print("The string does not end with the entered suffix.")
