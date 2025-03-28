44d655762a3bcb7be91c1ea1c4b16ba7673b0384
Chris <chriswell.mangahas@gmail.com>
2025-03-29 02:08:41 +0800

    Added pseudocode for Batch 6 - Program 6: add_space_program

---
+++ add_space_program.py
@@ -0,0 +1,6 @@
+#Batch 6 - Program 6
+#Add spaces at the end of the string based on how many is wanted by the user without using ljust()
+
+#ask the user to input a string and how many spaces they would like to add
+#add the spaces at the end of the string
+#display the result
