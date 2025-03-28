010cabc96deb244bcbab22f398a1bf4880d02b48
Chris <chriswell.mangahas@gmail.com>
2025-03-28 23:57:49 +0800

    Added new lines of code in no_prefix_program, and updated its respectice pseudocode.

--- no_prefix_program.py
+++ no_prefix_program.py
@@ -5,5 +5,10 @@
 string = input("Enter a string: ")
 prefix = input("Enter prefix you want to remove: ")
 
-#check if the string starts with the prefix and remove it from the string
+#check if the string starts with the prefix and remove it from the string by making a new string starting at the point after the prefix, otherwise keep the string the same if the prefix is not found
+if string.startswith(prefix):
+    new_string = string[len(prefix):]
+else:
+    new_string = string
+
 #display new string
