5555f7fa5d7cf4bad12b5df4a29b6b14d5ad84c0
Chris <chriswell.mangahas@gmail.com>
2025-03-29 00:55:17 +0800

    Added first lines of code in suffix_checker_program

--- suffix_checker_program.py
+++ suffix_checker_program.py
@@ -2,5 +2,8 @@
 #Check if the string ends with the entered suffix without using endswith()
 
 #ask the user to input a string and the suffix they want to check
+string = input("Enter a string: ")
+suffix = input("Enter suffix you want to check: ")
+
 #check if the end of the string matches with the suffix
 #display the result
