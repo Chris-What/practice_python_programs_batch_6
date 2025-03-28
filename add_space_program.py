6f3164f654229d5b242d40b0a8898a576b7dde3d
Chris <chriswell.mangahas@gmail.com>
2025-03-29 02:13:46 +0800

    Added first lines of code in add_space_program

--- add_space_program.py
+++ add_space_program.py
@@ -2,5 +2,8 @@
 #Add spaces at the end of the string based on how many is wanted by the user without using ljust()
 
 #ask the user to input a string and how many spaces they would like to add
+string = input("Enter a string: ")
+spaces = int(input("Enter amount of spaces to be added: "))
+
 #add the spaces at the end of the string
 #display the result
