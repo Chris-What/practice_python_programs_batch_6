73596da10b02936798b31ce737f34dc700084979
Chris <chriswell.mangahas@gmail.com>
2025-03-29 00:11:58 +0800

    Added new lines of code in lowercase_program, and updated its respective pseudocode.

--- lowercase_program.py
+++ lowercase_program.py
@@ -4,5 +4,12 @@
 #ask the user to input a string with uppercase letters
 string = input("Enter a string with uppercase letters: ")
 
-#convert the uppercase letters in the string into lowercase
+#convert the uppercase letters in the string into lowercase by using ASCII values
+new_string = ""
+for char in string:
+    if "A" <= char <= "Z":
+        new_string = chr(ord(char) + 32)
+    else:
+        new_string += char
+
 #display the new string
