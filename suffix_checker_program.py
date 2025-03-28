9a229c958fe44a4b615db4fab79e1fe51f29bcd9
Chris <chriswell.mangahas@gmail.com>
2025-03-29 00:53:10 +0800

    Added pseudocode for Batch 6 - Program 5: suffix_checker_program

---
+++ suffix_checker_program.py
@@ -0,0 +1,6 @@
+#Batch 6 - Program 5
+#Check if the string ends with the entered suffix without using endswith()
+
+#ask the user to input a string and the suffix they want to check
+#check if the end of the string matches with the suffix
+#display the result
