254115c0a3ae4ef911f14c333fa20fb474b125f2
Chris <chriswell.mangahas@gmail.com>
2025-03-28 23:54:21 +0800

    Added first lines of codes in no_prefix_program

--- no_prefix_program.py
+++ no_prefix_program.py
@@ -2,5 +2,8 @@
 #Remove a specified prefix in the beginning of a string without using removeprefix()
 
 #ask the user to input a string and the prefix they want to be removed
+string = input("Enter a string: ")
+prefix = input("Enter prefix you want to remove: ")
+
 #check if the string starts with the prefix and remove it from the string
 #display new string
