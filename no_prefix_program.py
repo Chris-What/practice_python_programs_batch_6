17465fd9ce00975dda41fce6084e56f8f3956fd9
Chris <chriswell.mangahas@gmail.com>
2025-03-28 23:51:49 +0800

    Added pseudocode for Batch 6 - Program 2: no_prefix_program

---
+++ no_prefix_program.py
@@ -0,0 +1,6 @@
+#Batch 6 - Program 2
+#Remove a specified prefix in the beginning of a string without using removeprefix()
+
+#ask the user to input a string and the prefix they want to be removed
+#check if the string starts with the prefix and remove it from the string
+#display new string
