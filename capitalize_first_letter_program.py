31744556b8d64fbf9dc58c029cd04d621e2ffb64
Chris <chriswell.mangahas@gmail.com>
2025-03-30 00:18:39 +0800

    Added new lines of code in capitalize_first_letter_program, and updated its respective pseudocode.

--- capitalize_first_letter_program.py
+++ capitalize_first_letter_program.py
@@ -4,5 +4,10 @@
 #ask the user to input a string
 string = input("Enter a string: ")
 
-#capitalize the first letter of the string and convert the rest into lowercase
+#capitalize the first letter of the string using ASCII values
+if len(string) > 0:
+    first_letter = string[0]
+    if "a" <= first_letter <= "z":
+        first_letter = chr(ord(first_letter) - 32)
+
 #display the new string
