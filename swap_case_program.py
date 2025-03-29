02efdbd34782c6da70fc8d910c4e5690b8458022
Chris <chriswell.mangahas@gmail.com>
2025-03-29 23:44:10 +0800

    Added first line of code in swap_case_program

--- swap_case_program.py
+++ swap_case_program.py
@@ -2,5 +2,7 @@
 #Swap lowercase letters to uppercase and vice versa without using swapcase()
 
 #ask the user to input a string
+string = input("Enter a string: ")
+
 #swap the cases of the characters in the string
 #display the new string
