93a978564b60f8b206ee0677cfb23b6c07cd012e
Chris <chriswell.mangahas@gmail.com>
2025-03-29 23:19:05 +0800

    Added first lines of code in center_space_program

--- center_space_program.py
+++ center_space_program.py
@@ -2,5 +2,8 @@
 #Center the string with spaces without using center()
 
 #ask the user to input a string and how many spaces would they like to add
+string = input("Enter a string: ")
+spaces = int(input("Enter amount of spaces to be added: "))
+
 #add the spaces to the left and right of the string
 #create and display a new string centered within the spaces
