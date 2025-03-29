76b3fa71892a697fd7eb7afa4ab4755eda198be8
Chris <chriswell.mangahas@gmail.com>
2025-03-30 01:07:41 +0800

    Added new lines of code in title_program, also updated two pseudocodes.

--- title_program.py
+++ title_program.py
@@ -5,9 +5,22 @@
 string = input("Enter a string: ")
 
 #split the words in the string and store every changed word
-word = string.split(" ")
+words = string.split(" ")
 new_words = []
 
-#capitalize the first letter of every word
-#convert every other letter into lowercase
+#capitalize the first letter of every word by using ASCII values
+for word in words:
+    if len(word) > 0:
+        first_letter = []
+        if "a" <= first_letter <= "z":
+            first_letter = chr(ord(first_letter) - 32)
+
+#convert every other letter into lowercase by using ASCII values
+        other_letters = ""
+        for char in words[1:]:
+            if "A" <= char <= "Z":
+                other_letters += chr(ord(char) + 32)
+            else:
+                other_letters += char
+
 #display the new string
