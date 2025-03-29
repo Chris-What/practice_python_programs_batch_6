c0b889ea9ab440591b4c1a4c5e60ca684c6e80ed
Chris <chriswell.mangahas@gmail.com>
2025-03-29 23:53:31 +0800

    Added new lines of code in swap_case_program, and updatd its respective pseudocode

--- swap_case_program.py
+++ swap_case_program.py
@@ -4,5 +4,14 @@
 #ask the user to input a string
 string = input("Enter a string: ")
 
-#swap the cases of the characters in the string
+#swap the cases of the characters in the string by using ASCII values
+new_string = ""
+for char in string:
+    if "A" <= char <= "Z":
+        new_string += chr(ord(char) + 32)
+    elif "a" <= char <= "z":
+        new_string += chr(ord(char) - 32)
+    else:
+        new_string += char
+        
 #display the new string
