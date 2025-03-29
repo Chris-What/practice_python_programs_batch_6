3fcb5a46d8e733fdaf21fc5400bc6f1a91bb6046
Chris <chriswell.mangahas@gmail.com>
2025-03-30 00:10:30 +0800

    Added first line of code in capitalize_first_letter_program

--- capitalize_first_letter_program.py
+++ capitalize_first_letter_program.py
@@ -2,5 +2,7 @@
 #Capitalize the first letter of the string and convert the rest into lowercase without using capitalize()
 
 #ask the user to input a string
+string = input("Enter a string: ")
+
 #capitalize the first letter of the string and convert the rest into lowercase
 #display the new string
