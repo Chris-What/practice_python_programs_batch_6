36901cc4df3b0dd9a5b86eb120c6c94ca831bc1d
Chris <chriswell.mangahas@gmail.com>
2025-03-29 23:41:45 +0800

    Added pseudocode for Batch 6 - Program 8: swap_case_program

---
+++ swap_case_program.py
@@ -0,0 +1,6 @@
+#Batch 6 - Program 8
+#Swap lowercase letters to uppercase and vice versa without using swapcase()
+
+#ask the user to input a string
+#swap the cases of the characters in the string
+#display the new string
