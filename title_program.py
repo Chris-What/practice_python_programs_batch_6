3cd48647b71a11fa50b1977338ab55afaeadb12c
Chris <chriswell.mangahas@gmail.com>
2025-03-30 00:51:31 +0800

    Added pseudocode for Batch 6 - Program 10: title_program

---
+++ title_program.py
@@ -0,0 +1,7 @@
+#Batch 6 - Program 10
+#Capitalize the first letter of every word in the string and convert every other letter into lowercase without using title()
+
+#ask the user to input a string
+#capitalize the first letter of every word
+#convert every other letter into lowercase
+#display the new string
