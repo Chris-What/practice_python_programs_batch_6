37013f58a5460eca5982fc99250933606bae45d1
Chris <chriswell.mangahas@gmail.com>
2025-03-29 23:24:45 +0800

    Added new lines of code in center_space_program, and updated its respective pseudocode.

--- center_space_program.py
+++ center_space_program.py
@@ -5,5 +5,12 @@
 string = input("Enter a string: ")
 spaces = int(input("Enter amount of spaces to be added: "))
 
-#add the spaces to the left and right of the string
+#add the spaces to the left and right of the string by evenly distributing them to each side to ensure the string is in the center
+if len(string) >= spaces:
+    new_string = string
+else:
+    total_spaces = spaces - len(string)
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+
 #create and display a new string centered within the spaces
