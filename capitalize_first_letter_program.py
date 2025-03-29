16c0c92ce82d6252459bc388da04d201f617eb8a
Chris <chriswell.mangahas@gmail.com>
2025-03-30 00:08:23 +0800

    Added pseudocode for Batch 6 - Program 9: capitalize_first_letter_program

---
+++ capitalize_first_letter_program.py
@@ -0,0 +1,6 @@
+#Batch 6 - Program 9
+#Capitalize the first letter of the string and convert the rest into lowercase without using capitalize()
+
+#ask the user to input a string
+#capitalize the first letter of the string and convert the rest into lowercase
+#display the new string
